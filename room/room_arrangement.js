31fe59d7a2478c5fa2acf27718394c058364afab
Richie Dix <rgchdx@gmail.com>
2025-05-20 10:44:38 -0500

    first commit

---
+++ room/room_arrangement.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var room = function() {
+    let canvas, gl, program;
+
+    // camera position and view
+    let eye = vec3(0.0, 1.0, 5.0);
+    let at = vec3(0.0, 1.0, 0.0);
+    let up = vec3(0.0, 1.0, 0.0);
+
+    // matrices
+    let fov = 60;
+    let near = 0.1;
+    let far = 100;
+
+    
+}
+
+stars();
